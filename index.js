70f66015cca8814bd61285b70abd26d8b32906c0
yusup159 <101506490+yusup159@users.noreply.github.com>
2025-10-20 15:57:44 +0700

    add connection to db

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const pool = require('./src/db');
+require('dotenv').config();
+
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-app.use(express.json());
+(async () => {
+  try {
+    const conn = await pool.getConnection();
+    console.log('✅ Koneksi ke MySQL berhasil!');
+    conn.release();
+  } catch (err) {
+    console.error('❌ Gagal konek ke MySQL:', err.message);
+  }
+})();
 
-app.get('/', (req, res) => {
-  res.send('Server Express berjalan 🚀');
+app.get('/', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT NOW() AS now');
+    res.send(`Server berjalan 🚀 — Waktu DB: ${rows[0].now}`);
+  } catch (err) {
+    console.error('Koneksi ke database gagal:', err.message);
+    res.status(500).send('Database error');
+  }
 });
 
 app.listen(PORT, () => {
